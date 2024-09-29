ff5c0ba7804bc487c81b4e8bcd55b9bd59d79654
Jhonatan <j.morao@aluno.ifsp.edu.br>
2024-09-29 19:00:10 +0000

    Primeiro commit do projeto Flask

---
+++ flask_app.py
@@ -0,0 +1,15 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World!'
+
+    @app.route('/user/<name>')
+    def user(name):
+        return '<h1>Hello, {}!</h1>'.format(name)
+
